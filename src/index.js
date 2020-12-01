d9c22c0691e7699cc8bff5ab6dbd3cad8ab01503
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2020-11-30 22:26:32 -0700

    initial push

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
+function App() {
+
+  const [count, setCount] = useState(0);
+  const [title,setTitle] = useState('hello');
+
+  return (
+    <div className= 'counter'>
+    <h1> React Counter</h1>
+    <p className= 'click_desc'>
+      your count is <span>{count > 0 ? count : 'IDK' }</span>
+    </p>
+
+    <div className = 'button_container'>
+
+      <button onClick={() => setCount(count + 1)}> +1 </button>
+      <button onClick={() => setCount( count * 5)}> *5 </button>
+      <button onClick={() => setCount(0)}> reset </button>
+    </div>
+    </div>
+  );
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
